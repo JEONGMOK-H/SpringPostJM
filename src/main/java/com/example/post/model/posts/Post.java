c84dcf2e5d8271e36c5086efa8a5484c375ad96a
jeongmok.h <massivenebula@gmail.com>
2025-01-23 10:09:10 +0900

    250123 아침Repository 날리기 전

--- src/main/java/com/example/post/model/posts/Post.java
+++ src/main/java/com/example/post/model/posts/Post.java
@@ -4,13 +4,27 @@ import java.time.LocalDateTime;
 
 import com.example.post.model.users.User;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
-
+@Entity
 @Data
 public class Post {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
+	
+	@Lob
 	private String content;
+	
+	@ManyToOne(fetch = FetchType.LAZY) // ManyToOne은 n+1 문제 방지를 위해 LAZA 타입으로 하는게 좋다
+	@JoinColumn(name="user_id") // ManyToOne에는 필수로 JoinColumn 지정(외래키 그 컬럼이름 지정)
 	private User user;
 	private int views;
 	private LocalDateTime createTime;

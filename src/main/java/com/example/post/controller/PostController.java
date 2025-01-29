62050c0ff390c1d9e268a499ce71c65d3a7b3d3a
jeongmok.h <massivenebula@gmail.com>
2025-01-29 17:26:22 +0900

    0129  at the Starbucks

--- src/main/java/com/example/post/controller/PostController.java
+++ src/main/java/com/example/post/controller/PostController.java
@@ -1,6 +1,27 @@
 package com.example.post.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
 public class PostController {
 
-    //
+	// 글 쓰기
+    @GetMapping(path = "posts/create")
+	public String savaPost(){
+		log.info("---- Get : savePost()");
+
+		return "posts/create";
+	}
+
+	// 글 목록보기
+	@GetMapping(path="posts")
+	public String postList(){
+		log.info("---- Get : savelist()");
+
+		return "posts/list";
+	}
+
 }
